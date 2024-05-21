0ce953192c0622722721d6c5cc9cd6abe357b36a
Fernando Salas <luisfernandosalasg@gmail.com>
2024-05-21 18:15:55 -0500

    frontend/Home UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { NegocioComponent } from './components/negocio/negocio.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'negocio', component: NegocioComponent },
 ];
